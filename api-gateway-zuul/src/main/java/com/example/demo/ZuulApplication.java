7654ce8ea354e4a8eb2a8f9dd23d5f16fef80860
lixj-zj <lixj_zj@163.com>
2021-10-15 18:18:50 +0800

    zuul gateway

--- api-gateway-zuul/src/main/java/com/example/demo/ZuulApplication.java
+++ api-gateway-zuul/src/main/java/com/example/demo/ZuulApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableRetry
 public class ZuulApplication {
 
     public static void main(String[] args) {
