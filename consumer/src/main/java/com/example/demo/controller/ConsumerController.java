0a912d7d8e2cca9c8f59d00faa25af1ef494327f
lixj-zj <lixj_zj@163.com>
2021-10-29 16:35:32 +0800

    feign 优化；

--- consumer/src/main/java/com/example/demo/controller/ConsumerController.java
+++ consumer/src/main/java/com/example/demo/controller/ConsumerController.java
@@ -1,31 +1,31 @@
-package com.example.demo.controller;
+    package com.example.demo.controller;
 
-import com.example.demo.feign.ConsumerRemote;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import com.example.demo.feign.ConsumerRemote;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RestController;
 
-import java.rmi.Remote;
+    import java.rmi.Remote;
 
-/**
- * @Description: --------------------------------------
- * @ClassName: ConsumerController.java
- * @Date: 2021/9/30 17:03
- * @SoftWare: IntelliJ IDEA
- * --------------------------------------
- * @Author: lixj
- * @Contact: lixj_zj@163.com
- **/
-@RestController
-public class ConsumerController {
+    /**
+     * @Description: --------------------------------------
+     * @ClassName: ConsumerController.java
+     * @Date: 2021/9/30 17:03
+     * @SoftWare: IntelliJ IDEA
+     * --------------------------------------
+     * @Author: lixj
+     * @Contact: lixj_zj@163.com
+     **/
+    @RestController
+    public class ConsumerController {
 
-    @Autowired
-    ConsumerRemote remote;
+        @Autowired
+        ConsumerRemote remote;
 
-    @RequestMapping("/hello2/{name}")
-    public String index(@PathVariable("name") String name) {
-        return remote.hello2(name);
+        @RequestMapping("/hello2/{name}")
+        public String index(@PathVariable("name") String name) {
+            return remote.hello2(name);
+        }
+
     }
-
-}
